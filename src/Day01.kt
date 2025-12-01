eb396547ef0ecb5e78d08c83643603fadc18b4fa
karloti <skarloti@gmail.com>
2025-12-01 14:47:10 +0200

    Feat: Solve Day 01 parts 1 & 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,29 @@
+private val String.sign: Int get() = (if (first() == 'L') -1 else 1)
+private val String.num: Int get() = drop(1).toInt()
+private fun Int.pos(num: Int): Int = ((this + num) % 100 + 100) % 100
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.fold(50 to 0) { (total, zeroHits), line ->
+            val pos = total.pos(line.sign * line.num)
+            pos to if (pos == 0) zeroHits + 1 else zeroHits
+        }.second
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.fold(50 to 0) { (total, zeroHits), line ->
+            val list = List(line.num) { total.pos((it + 1) * line.sign) }
+            list.last() to (zeroHits + list.count { it == 0 })
+        }.second
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 3)
+    check(part2(testInput) == 6)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+    check(part1(input) == 1123)
+    check(part2(input) == 6695)
 }
